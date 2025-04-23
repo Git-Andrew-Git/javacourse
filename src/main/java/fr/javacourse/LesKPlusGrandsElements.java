373679b46ffd4377c6564977ab55d05fc9dfd70a
Git-Andrew-Git <177481666+Git-Andrew-Git@users.noreply.github.com>
2025-04-23 12:55:05 +0200

    Day 7 LesKPlusGrandsElements

--- src/main/java/fr/javacourse/LesKPlusGrandsElements.java
+++ src/main/java/fr/javacourse/LesKPlusGrandsElements.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class LesKPlusGrandsElements {
 
-    public static void quickSort(int[] arr, int low, int high) {
+    public static void quickSort(ArrayList<Integer> arr, int low, int high) {
         if (low < high) {
             int pi = partition(arr, low, high);
             quickSort(arr, low, pi - 1);
@@ -12,35 +12,32 @@ public class LesKPlusGrandsElements {
         }
     }
 
-    private static int partition(int[] arr, int low, int high) {
-        int pivot = arr[high];
+    private static int partition(ArrayList<Integer> arr, int low, int high) {
+        int pivot = arr.get(high);
         int i = low - 1;
         for (int j = low; j < high; j++) {
-            if (arr[j] <= pivot) {
+            if (arr.get(j) <= pivot) {
                 i++;
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+                int temp = arr.get(i);
+                arr.set(i, arr.get(j));
+                arr.set(j, temp);
             }
         }
-        int temp = arr[i + 1];
-        arr[i + 1] = arr[high];
-        arr[high] = temp;
+        int temp = arr.get(i + 1);
+        arr.set(i + 1, arr.get(high));
+        arr.set(high, temp);
         return i + 1;
     }
+
     public static ArrayList<Integer> lesKPlusGrandsElements(int k, ArrayList<Integer> tableau){
 
         if (tableau.size() >= k) {
             ArrayList<Integer> newtableau = new ArrayList<>(k);
-            int moitie;
-            int indicenewtableau = 0;
+
 //          tri rapide
             quickSort(tableau, 0, tableau.size()-1);
-            for (int i = 0; i < tableau.size(); i++) {
-                moitie = tableau.size()/2;
-                if (tableau.get(i) < tableau.get(moitie)) {
-
-                }
+            for (int i = 1; i <= k; i++) {
+                newtableau.set(i-1, tableau.get(i));
             }
             return newtableau;
         } else {
