cb7c2494e16c013c0590020a46c1744036103c1f
Git-Andrew-Git <177481666+Git-Andrew-Git@users.noreply.github.com>
2025-04-23 11:57:07 +0200

    Day 7 LesKPlusGrandsElements

--- src/main/java/fr/javacourse/LesKPlusGrandsElements.java
+++ src/main/java/fr/javacourse/LesKPlusGrandsElements.java
@@ -3,21 +3,43 @@ package fr.javacourse;
 import java.util.ArrayList;
 
 public class LesKPlusGrandsElements {
+
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        return i + 1;
+    }
     public static ArrayList<Integer> lesKPlusGrandsElements(int k, ArrayList<Integer> tableau){
 
         if (tableau.size() >= k) {
             ArrayList<Integer> newtableau = new ArrayList<>(k);
+            int moitie;
             int indicenewtableau = 0;
-            int moitie = tableau.size()/2;
-//          tri par sélection
+//          tri rapide
+            quickSort(tableau, 0, tableau.size()-1);
             for (int i = 0; i < tableau.size(); i++) {
-                if (i == 0) {
-                    newtableau.set(i, tableau.get(i));
-                    indicenewtableau++;
-                } else {
-                    if (tableau.get(i) < newtableau.get(indicenewtableau)) {
-                        
-                    }
+                moitie = tableau.size()/2;
+                if (tableau.get(i) < tableau.get(moitie)) {
+
                 }
             }
             return newtableau;
